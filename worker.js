9f0916a01d0930773b5e092ecbd5fc8a526f153a
Vistril <scamomilegm@gmail.com>
2019-12-05 22:51:18 -0800

    forgot formatting

--- worker.js
+++ worker.js
@@ -1,13 +1,13 @@
 class s1Tools {
     constructor() { /*not needed?*/ }
 
-    async randomUser(i) {
-        await i.guild.members.map(m => m.displayName || m.username);
+    randomUser(i) {
+        return "***(" + i.guild.members.map(m => m.displayName || m.username) + ")***"
     }
 
     randomFace(i) {
         let faces = ['blabla put faces here >:('];
-        return faces[~~(Math.random () * faces.length)];
+        return "**" + faces[~~(Math.random () * faces.length)] + "**"
     }
 }
 
