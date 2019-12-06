3b0103fd13f1d8fce34bf6efad3eb2ae57686e30
Vistril <scamomilegm@gmail.cmo>
2019-12-05 23:35:55 -0800

    gay removed

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 const client = new (require('discord.js')).Client();
 const { randomFace, randomUser } = new (require('./worker.js'))();
-const token = require('fs').readFileSync('token.txt', {encoding: 'utf8'});
+const token = require('fs').readFileSync('./token.txt', {encoding: 'utf8'});
 const triggers = ['<@652373244539699200>', '<@!652373244539699200>', "@someone"];
 client.on('a', m => {
     let first = m.content.split(' ')[0].toLowerCase();
