023315942862b6c1214624458baf54abaf8b75fa
Vistril <38549907+Vistril@users.noreply.github.com>
2019-12-09 13:47:22 -0700

    even more gross

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ client.on('message', m => {
     console.log(m.content);
     let first = m.content.split(' ')[0].toLowerCase();
     let second = m.content.slice(first.length).trim();
-    let a = 0, b = 10;
-    let formatted = m.content.replace(/@someone/g, match => a++ < b ? `${randomFace} ${randomUser()} ${second}` : match);
-    m.reply(formatted);
+    let a = 0, b = 9;
+    let formatted = m.content.replace(/@someone/g, `${randomFace} ${randomUser()} ${second}`);
+                     .replace(/@someone/g, match => a++ < b ? `${randomFace} ${randomUser()}` : match);   m.reply(formatted);
     if (first == 's1-help') {
         m.reply(':warning: Hello! The bot is still under some construction, so the help command is mostly empty for now!\nCurrent Commands: `@someone`, `s1-ping`');
     } else if (first == 's1-ping') {
