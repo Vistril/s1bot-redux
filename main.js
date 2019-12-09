a2d1b3e9c5411db27b006c1e68e73a45a0767434
Vistril <38549907+Vistril@users.noreply.github.com>
2019-12-09 13:44:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ client.on('message', m => {
     console.log(m.content);
     let first = m.content.split(' ')[0].toLowerCase();
     let second = m.content.slice(first.length).trim();
-    let n = 0, n = 4;
-    let formatted = m.content.replace(/@someone/g, match => n++ < N ? `${randomFace} ${randomUser()} ${second}` : match);
+    let a = 0, b = 4;
+    let formatted = m.content.replace(/@someone/g, match => a++ < b ? `${randomFace} ${randomUser()} ${second}` : match);
     m.reply(formatted);
     if (first == 's1-help') {
         m.reply(':warning: Hello! The bot is still under some construction, so the help command is mostly empty for now!\nCurrent Commands: `@someone`, `s1-ping`');
